07000747701e5d37fc4b72e99eecddfdf44d111c
Noel <castillo752x@gmail.com>
2019-11-14 14:17:02 -0700

    sync

--- PetBnB/src/main/java/com/skilldistillery/petbnb/PetBnBApplication.java
+++ PetBnB/src/main/java/com/skilldistillery/petbnb/PetBnBApplication.java
@@ -2,8 +2,10 @@ package com.skilldistillery.petbnb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.skilldistillery.petbnb")
 public class PetBnBApplication {
 
 	public static void main(String[] args) {
