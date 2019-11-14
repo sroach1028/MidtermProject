a30268733e171e1373cda679b37ab1d06e935e6b
Noel <castillo752x@gmail.com>
2019-11-14 14:31:22 -0700

    more initial set ups

--- PetBnB/src/main/java/com/skilldistillery/petbnb/PetBnBApplication.java
+++ PetBnB/src/main/java/com/skilldistillery/petbnb/PetBnBApplication.java
@@ -3,10 +3,17 @@ package com.skilldistillery.petbnb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EntityScan("com.skilldistillery.petbnb")
-public class PetBnBApplication {
+public class PetBnBApplication extends SpringBootServletInitializer{
+	
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(PetBnBApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetBnBApplication.class, args);
