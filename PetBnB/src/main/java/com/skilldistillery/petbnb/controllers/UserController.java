ecb6a145c79c265c583f404e4d9fbae5f82c7cb3
Noel <castillo752x@gmail.com>
2019-11-15 15:02:00 -0700

    basic web templates

--- PetBnB/src/main/java/com/skilldistillery/petbnb/controllers/UserController.java
+++ PetBnB/src/main/java/com/skilldistillery/petbnb/controllers/UserController.java
@@ -1,8 +1,19 @@
 package com.skilldistillery.petbnb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
 
+
+	@RequestMapping(path = "/")
+	public String home() {
+		return "home";
+	}
+	
+	@RequestMapping(path = "coming.do")
+	public String comingSoon() {
+		return "coming-soon";
+	}
 }
