7df783dbeb1a674d2b9def301e1fcea79b4824fc
Noel <castillo752x@gmail.com>
2019-11-21 12:27:31 -0700

    pictures stay after updating pet

--- PetBnB/src/main/java/com/skilldistillery/petbnb/data/PettrDAOImpl.java
+++ PetBnB/src/main/java/com/skilldistillery/petbnb/data/PettrDAOImpl.java
@@ -71,7 +71,7 @@ public class PettrDAOImpl implements PettrDAO {
 		updatedPet.setBreed(pet.getBreed());
 		updatedPet.setSpecialNeeds(pet.getSpecialNeeds());
 		updatedPet.setDescription(pet.getDescription());
-		updatedPet.setPetImages(pet.getPetImages());
+//		updatedPet.setPetImages(pet.getPetImages());
 
 		em.flush();
 
